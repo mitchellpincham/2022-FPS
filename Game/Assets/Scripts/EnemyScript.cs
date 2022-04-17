c84a5df2f68e92fd0c479752ad709ab514dd2f0e
Mitchell Pincham <mitch.pincham@gmail.com>
2022-04-18 11:37:23 +1200

    Enemy health display
    I added a unity slider above the enemy that displays the percentage of health that they have. I also made the enemy look at the player at all times so the slider looks normal.

--- Game/Assets/Scripts/EnemyScript.cs
+++ Game/Assets/Scripts/EnemyScript.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
 
     public float maxHealth;
     public float health;
+    public Transform player;
 
     void Start() {
         health = maxHealth;
+    }
+
+    void Update() {
+        Slider healthSlider = (Slider)gameObject.GetComponentInChildren(typeof(Slider));
+
+        healthSlider.value = health / maxHealth;
+
+        //transform.LookAt(player);
+        float dx = player.position.x - transform.position.x;
+        float dz = player.position.z - transform.position.z;
+
+        float ang = (float)Math.Atan2(dx, dz) * 180 / (float)Math.PI;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, ang, 0));
     }
 
     public void BulletHit(float damage) {
