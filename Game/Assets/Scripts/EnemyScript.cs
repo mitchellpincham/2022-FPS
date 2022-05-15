b0375297934ae594e24ea59cdb1a0624d6e6d259
Mitchell Pincham <mitch.pincham@gmail.com>
2022-05-16 09:02:31 +1200

    Added the NavMesh
    I set up the NavMesh for the enemy, there is an empty transform that the enemy will path find to.

--- Game/Assets/Scripts/EnemyScript.cs
+++ Game/Assets/Scripts/EnemyScript.cs
@@ -3,26 +3,64 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class EnemyScript : MonoBehaviour
 {
 
     public float maxHealth;
     public float health;
-    public Transform player;
+    public GameObject player;
+    private NavMeshAgent navMeshAgent;
+    public Transform movePositionTransform;
 
-    void Start() {
+    // patroling
+    public Vector3 walkpoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // states
+    public float sightRange, attackRange;
+
+    void Awake() {
         health = maxHealth;
+
+        // set the navmesh object
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Patroling() {
+
+    }
+
+    void ChasePlayer() {
+
+    }
+
+    void AttackPlayer() {
+
     }
 
     void Update() {
+
+        navMeshAgent.destination = movePositionTransform.position;
+
+
+
+
+        // set the health bar value
         Slider healthSlider = (Slider)gameObject.GetComponentInChildren(typeof(Slider));
 
         healthSlider.value = health / maxHealth;
 
-        //transform.LookAt(player);
-        float dx = player.position.x - transform.position.x;
-        float dz = player.position.z - transform.position.z;
+        // look at player along two axis;
+        //transform.LookAt(player.transform);
+        float dx = player.transform.position.x - transform.position.x;
+        float dz = player.transform.position.z - transform.position.z;
 
         float ang = (float)Math.Atan2(dx, dz) * 180 / (float)Math.PI;
 
