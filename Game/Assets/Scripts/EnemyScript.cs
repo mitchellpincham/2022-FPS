ebace8cfee5a032dc7e9da6144eb22271504ea9e
Mitchell Pincham <mitch.pincham@gmail.com>
2022-05-19 10:38:26 +1200

    Enemy states
    I added patrol, chase and attack states for the enemy. Now the enemy will check if it can see the player, if it can't then it will 'patrol', so go to random points. Then if it is in sight range of player it will move to player. Then if it is within 'attack' range then it will stand still.

--- Game/Assets/Scripts/EnemyScript.cs
+++ Game/Assets/Scripts/EnemyScript.cs
@@ -11,11 +11,13 @@ public class EnemyScript : MonoBehaviour
     public float maxHealth;
     public float health;
     public GameObject player;
-    private NavMeshAgent navMeshAgent;
+    private NavMeshAgent agent;
     public Transform movePositionTransform;
 
+    public LayerMask whatIsGround, whatIsPlayer;
+
     // patroling
-    public Vector3 walkpoint;
+    public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
 
@@ -25,31 +27,84 @@ public class EnemyScript : MonoBehaviour
 
     // states
     public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
 
     void Awake() {
         health = maxHealth;
 
         // set the navmesh object
-        navMeshAgent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     void Patroling() {
+        // if no walkpoint is set then set one
+        if (!walkPointSet) {
+            SearchWalkPoint();
+        }
+        // walk to walkpoint
+        if (walkPointSet) {
+            agent.SetDestination(walkPoint);
+        }
 
+        Vector3 walkPointDistance = transform.position - walkPoint;
+
+        // if close to walkpoint
+        if (walkPointDistance.magnitude < 1f) {
+            walkPointSet = false;
+        }
+    }
+
+    void SearchWalkPoint() {
+        // find random point in the range and navmesh
+        float randomZ = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        float randomX = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+        
+        // set the new walkpoint
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        // if the walkpoint is good, then walkPointSet = true.
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) {
+            walkPointSet = true;
+        }
     }
 
     void ChasePlayer() {
-
+        // if player is in sight, then move towards him.
+        agent.SetDestination(player.transform.position);
     }
 
     void AttackPlayer() {
-
+        // if the player is in the attack range then dont move and shoot him.
+        agent.SetDestination(transform.position);
     }
 
     void Update() {
 
-        navMeshAgent.destination = movePositionTransform.position;
+        //agent.destination = movePositionTransform.position;
 
+        // distance to player
+        Vector3 playerDistance = player.transform.position - transform.position;
 
+        playerInAttackRange = false;
+        playerInSightRange = false;
+
+        if (playerDistance.magnitude < attackRange) {
+            playerInAttackRange = true;
+        } 
+        if (playerDistance.magnitude < sightRange) {
+            playerInSightRange = true;
+        }
+
+        if (!playerInSightRange && !playerInAttackRange) {
+            Debug.Log("patroling");
+            Patroling();
+        } else if (!playerInAttackRange) {
+            Debug.Log("chasing");
+            ChasePlayer();
+        } else {
+            Debug.Log("attacking");
+            AttackPlayer();
+        }
 
 
         // set the health bar value
---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // public bool isPaused;
+
+    // void Awake() {
+    //     PauseMenu.SetActive(false);
+    //     isPaused = false;
+    // }
+
+    // void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Escape)) {
+    //         if (isPaused) {
+    //             ResumeGame();
+    //         } else {
+    //             PauseGame();
+    //         }
+    //     }
+    // }
+
+    // void PauseGame() {
+    //     PauseMenu.SetActive(true);
+    //     Time.timescale = 0f;
+    //     isPaused = true;
+    // }
+
+    // void ResumeGame() {
+    //     PauseMenu.SetActive(false);
+    //     Time.timescale = 1f;
+    //     isPaused = false;
+    // }
+}
